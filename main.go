233d0130400fceb3fa751d5ee1adaaee40f5b9ee
Kyle Jensen <kjensen19@gmail.com>
2023-01-07 21:57:25 -0600

    Environment variable for db solved, query now working

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	// "log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
+	//Environment Variables are set in cmd line with $export KEY:VALUE, then
 	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
@@ -20,8 +29,40 @@ func main() {
 	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 
-	fmt.Println(greeting)
+	fmt.Printf(greeting)
+	albums, err := albumsByArtist("John Coltrane", dbpool)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "find album failed: %v\n", err)
+		os.Exit(3)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string, dbpool *pgxpool.Pool) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := dbpool.Query(context.Background(), "select * from album where artist=$1", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
+
+func albumByID(id int64, dbpool *pgxpool.Pool) (Album, error)
